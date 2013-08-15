175ef34590df5f756d6de299ace7e5de111ef370
balthazar de lavergne <balthazar.delavergne@gmail.com>
2013-08-15 08:24:40 +0000

    small change

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs= require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+
+
 app.get('/', function(request, response) {
-    var fs= require('fs');
-    var buf =fs.readFileSync('index.html');
+
     response.send(buf.toString('utf-8'));
 });
 
