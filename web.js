502f84788f96e00820b50ed01cda202861ba7573
balthazar de lavergne <balthazar.delavergne@gmail.com>
2013-08-15 08:05:33 +0000

    added fs in the functon

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var fs= require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 
 app.get('/', function(request, response) {
+    var fs= require('fs');
     buf = new Buffer(fs.readFileSync('index.html'));
-    response.send('buf.toString('utf-8')');
+    response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
