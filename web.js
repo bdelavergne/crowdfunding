f0e7dd7292d16f63f7a761385cc71de806285782
balthazar de lavergne <balthazar.delavergne@gmail.com>
2013-08-15 07:56:02 +0000

    new try

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+var fs= require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
@@ -5,7 +6,7 @@ app.use(express.logger());
 buf = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send('buf.toString()');
+  response.send('buf.toString('utf-8')');
 });
 
 var port = process.env.PORT || 5000;
